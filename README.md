2e844dde6c5df273e0823476d9de800101c5cb05
Pasquale Muscettola <pasquale.muscettola@gmail.com>
2018-01-24 17:16:39 +0100

    Sreenshot added to readme

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
+## Social-Card
+
+Social Card is a React component displaying a card item for a fake social network.
+
+![Example of a SocialCard](screenshots/social-card-example.png 'A simple SocialCard example')
 
 This project is the first of a series of example projects using ReactJS. The overall plan is to develop each of the example project found in [this article](https://daveceddia.com/react-practice-projects/) (which I do not own).
 
 Its purpose is to track my progress into getting used to properly develop using ReactJS.
-
-## Social-Card
-
-Social Card is a React component displaying a card item for a fake social network.
--- src/App.js
+++ src/App.js
@@ -9,19 +9,6 @@ class App extends Component {
       <div className="App">
         <SocialCard>
           <CardHeader
-            src="https://image.freepik.com/free-photo/cardboard-wallpaper-template_1194-6785.jpg"
-            alt="Cardboard"
-          />
-          <CardBody title="Cardboard">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            lacus nibh, aliquet eget blandit eget, elementum nec dui. Fusce
-            ultrices eros nec magna suscipit, ac lobortis nunc auctor.
-          </CardBody>
-          <CardFooter />
-        </SocialCard>
-
-        <SocialCard>
-          <CardHeader
             src="https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5222667836001_5214872539001-vs.jpg?pubId=5104226627001&videoId=5214872539001"
             alt="Morocco"
           />
